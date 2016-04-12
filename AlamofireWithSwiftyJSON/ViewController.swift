520c0ab6c82946c5a893858aa39f698e90f94a8d
Ashish Kakkad <ashishkakkad8@gmail.com>
2016-04-12 16:24:25 +0530

    Sample of Alamofire with SwiftyJSON

---
+++ AlamofireWithSwiftyJSON/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  AlamofireWithSwiftyJSON
+//
+//  Created by MAC-186 on 4/12/16.
+//  Copyright © 2016 Kode. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tblJSON: UITableView!
+    var arrRes = [[String:AnyObject]]() //Array of dictionary
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Alamofire.request(.GET, "http://api.androidhive.info/contacts/").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                if let resData = swiftyJsonVar["contacts"].arrayObject {
+                    self.arrRes = resData as! [[String:AnyObject]]
+                }
+                if self.arrRes.count > 0 {
+                    self.tblJSON.reloadData()
+                }
+            }
+        }
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("jsonCell")!
+        var dict = arrRes[indexPath.row]
+        cell.textLabel?.text = dict["name"] as? String
+        cell.detailTextLabel?.text = dict["email"] as? String
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrRes.count
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
